fdbe4349e0c0779cf53aaf9902369b9b69833de0
hayas1 <h4ystack@gmail.com>
2024-04-23 15:24:14 +0000

    tokei without fs

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,8 +1,17 @@
 use yew::prelude::*;
 
+use crate::loc::get_statistics;
+
 #[function_component(App)]
 pub fn app() -> Html {
+    let languages = get_statistics("dummy", &Default::default());
+    let rust = &languages[&tokei::LanguageType::Rust];
+
     html! {
-        <h1>{ "Hello World" }</h1>
+        <>
+            <h1>{ "Hello World" }</h1>
+            <p>{ format!("Lines of code: {}", rust.code) }</p>
+            <p>{ format!("rust: {:?}", rust) }</p>
+        </>
     }
 }
