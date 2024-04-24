0a04118e6108ddb114c097029f90f07d35a4e54e
hayas1 <h4ystack@gmail.com>
2024-04-24 12:46:55 +0000

    pass `--public-url`

--- tools/prod-build.sh
+++ tools/prod-build.sh
@@ -5,4 +5,5 @@ scripts=$(dirname "$(realpath "$0")")
 
 repo=$(dirname "$(dirname "$(realpath "$0")")")
 
-trunk build --release --dist "$repo"/target/public
+PUBLIC_URL="${PUBLIC_URL:-"http://127.0.0.1:8080"}"
+trunk build --release --dist "$repo"/target/public --public-url "$PUBLIC_URL"
