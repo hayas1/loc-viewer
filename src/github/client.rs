4beb0ba90e2741e8e8795e696cb3c2d42f062daa
hayas1 <h4ystack@gmail.com>
2024-04-27 14:57:52 +0000

    cannot compile
    `*mut u8` cannot be sent between threads safely
    within `impl Future<Output = Result<http::Response<String>, anyhow::Error>>`, the trait `Send` is not implemented for `*mut u8`, which is required by `impl Future<Output = Result<http::Response<String>, anyhow::Error>>: Send`
    required because it captures the following types: `gloo::gloo_net::http::Request`, `web_sys::features::gen_Request::Request`, `wasm_bindgen_futures::js_sys::Object`, `JsValue`, `JsFuture`
    required because it captures the following types: `http::request::Parts`, `String`, `impl Future<Output = Result<gloo::gloo_net::http::Response, gloo::gloo_net::Error>>`

--- src/github/client.rs
+++ src/github/client.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
-use octocrab::models;
+use http::Uri;
+use octocrab::{models, service::middleware::base_uri::BaseUriLayer, AuthState};
 use url::Url;
 
 use super::repository::GitHubRepository;
@@ -21,9 +22,46 @@ impl GitHubApiClient {
         Ok(url)
     }
 
-    pub async fn get_repository(&self) -> Result<models::Repository> {
+    pub async fn get_repository_(&self) -> Result<models::Repository> {
         let GitHubRepository { owner, repo } = &self.repository;
         let path = format!("/repos/{owner}/{repo}");
         Ok(gloo::net::http::Request::get(self.endpoint(&path)?.as_str()).send().await?.json().await?)
+    }
+
+    async fn handle(request: http::Request<String>) -> Result<http::Response<String>> {
+        let (parts, body) = request.into_parts();
+        let method = match parts.method {
+            http::Method::GET => gloo::net::http::Method::GET,
+            http::Method::POST => gloo::net::http::Method::POST,
+            http::Method::PUT => gloo::net::http::Method::PUT,
+            http::Method::DELETE => gloo::net::http::Method::DELETE,
+            _ => todo!(), // TODO
+        };
+        let response = gloo::net::http::RequestBuilder::new(&parts.uri.to_string())
+            .method(method)
+            .body(body)
+            .unwrap()
+            .send()
+            .await
+            .unwrap();
+        Ok(hyper::Response::builder()
+            .status(response.status())
+            .body(response.body().unwrap().as_string().unwrap())
+            .unwrap())
+        // let response = http::Response::new("\"Hello, World!\"".to_string());
+        // Ok(response)
+    }
+
+    pub async fn get_repository(&self) -> Result<models::Repository> {
+        let GitHubRepository { owner, repo } = &self.repository;
+        let http_client = tower::service_fn(Self::handle);
+        let octo = octocrab::OctocrabBuilder::new_empty()
+            .with_service(http_client)
+            .with_layer(&BaseUriLayer::new(Uri::from_static("https://api.github.com"))) // !Send ...
+            // .with_layer(&ExtraHeadersLayer::new(Arc::new(vec![(USER_AGENT, "octocrab".parse().unwrap())])))
+            .with_auth(AuthState::None)
+            .build() // Buffer::new use tokio::spawn ...
+            .unwrap();
+        Ok(octo.repos(owner, repo).get().await?)
     }
 }
