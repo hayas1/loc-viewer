f0ae20d143e516a3388728b573333964108ad574
hayas1 <h4ystack@gmail.com>
2024-05-11 00:11:53 +0900

    width

--- src/components/statistics.rs
+++ src/components/statistics.rs
@@ -40,7 +40,7 @@ pub fn statistics_page(host: &String, owner: &String, repo: &String) -> HtmlResu
                     <StatisticsView repository={repository}/>
                 </Suspense>
             </Pane>
-            <Pane class={classes!("p-6", "w-max", "max-w-xs", "flex", "flex-col", "justify-start")}>
+            <Pane class={classes!("p-6", "max-w-xs", "flex", "flex-col", "justify-start")}>
                 <div class={classes!("h-full", "w-full", "flex", "flex-col", "justify-start")}>
                     <label for="repository-url" class={classes!("w-full")}>{"URL"}</label>
                     <div class={classes!("w-full", "pt-4")}>
