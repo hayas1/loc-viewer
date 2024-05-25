982059088cbc8de6cf5a44aa1b7cb53b7cf716d7
hayas1 <h4ystack@gmail.com>
2024-05-25 17:48:16 +0900

    public url

--- scripts/prod-build.sh
+++ scripts/prod-build.sh
@@ -5,5 +5,5 @@ scripts=$(dirname "$(realpath "$0")")
 
 repo=$(dirname "$(dirname "$(realpath "$0")")")
 
-PUBLIC_URL="${PUBLIC_URL:-"http://127.0.0.1:8080"}"
+PUBLIC_URL="${PUBLIC_URL:-"/tokei-toukei/"}"
 trunk build --release --dist "$repo"/target/public --public-url "$PUBLIC_URL"
